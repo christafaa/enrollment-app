1d06f8a47463b55a6ca20462a7da164d10497ebb
Chris Alberti <christopherjalberti@gmail.com>
2018-07-05 18:55:30 -0400

    created new forms for students and teachers

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -2,4 +2,12 @@ class StudentController < ApplicationController
   get '/students/new' do
     erb :'students/new'
   end
+
+  post '/students' do
+    if params[:name].empty? || params[:username].empty? || params[:password].empty?
+      redirect '/students/new'
+    else
+      Student.create(params)
+    end
+  end
 end
--- app/controllers/teacher_controller.rb
+++ app/controllers/teacher_controller.rb
@@ -2,4 +2,12 @@ class TeacherController < ApplicationController
   get '/teachers/new' do
     erb :'teachers/new'
   end
+
+  post '/teachers' do
+    if params[:name].empty? || params[:username].empty? || params[:password].empty?
+      redirect '/teachers/new'
+    else
+      Teacher.create(params)
+    end
+  end
 end

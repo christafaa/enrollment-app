603131703af37e13948e4fc45d4a4587905bdd85
Chris Alberti <christopherjalberti@gmail.com>
2018-07-06 13:47:42 -0400

    add teacher edit functionality

--- app/controllers/teacher_controller.rb
+++ app/controllers/teacher_controller.rb
@@ -23,7 +23,12 @@ class TeacherController < ApplicationController
 
   get '/teachers/:slug/edit' do
     @teacher = Teacher.find_by_slug(params[:slug])
-    erb :'teachers/edit'
+
+    if session[:user_id] == @teacher.id
+      erb :'teachers/edit'
+    else
+      redirect '/'
+    end
   end
 
   patch '/teachers/:slug' do
@@ -31,6 +36,8 @@ class TeacherController < ApplicationController
     teacher.name = params[:name] unless params[:name].empty?
     teacher.username = params[:username] unless params[:username].empty?
     teacher.password = params[:password] unless params[:password].empty?
+    teacher.alma_mater = params[:alma_mater] unless params[:alma_mater].empty?
+    teacher.years_experience = params[:years_experience] unless params[:years_experience].empty?
     teacher.save
     redirect "/teachers/#{teacher.slug}"
   end

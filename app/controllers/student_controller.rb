5836cb62d6f995a97b2207f531e56d5cc40550be
Chris Alberti <christopherjalberti@gmail.com>
2018-07-06 13:15:45 -0400

    add student edit functionality

--- app/controllers/student_controller.rb
+++ app/controllers/student_controller.rb
@@ -4,13 +4,13 @@ class StudentController < ApplicationController
     erb :'students/index'
   end
 
+  get '/students/new' do
+    erb :'students/new'
+  end
+
   get '/students/:student_name' do
     @student = Student.find_by_slug(params[:student_name])
     erb :'students/show'
-  end
-
-  get '/students/new' do
-    erb :'students/new'
   end
 
   post '/students' do
@@ -19,6 +19,20 @@ class StudentController < ApplicationController
     else
       Student.create(params)
     end
+  end
+
+  get '/students/:slug/edit' do
+    @student = Student.find_by_slug(params[:slug])
+    erb :'students/edit'
+  end
+
+  patch '/students/:slug' do
+    student = Student.find_by_slug(params[:slug])
+    student.name = params[:name] unless params[:name].empty?
+    student.username = params[:username] unless params[:username].empty?
+    student.password = params[:password] unless params[:password].empty?
+    student.save
+    redirect "/students/#{student.slug}"
   end
 
 end
--- app/controllers/teacher_controller.rb
+++ app/controllers/teacher_controller.rb
@@ -4,13 +4,13 @@ class TeacherController < ApplicationController
     erb :'teachers/index'
   end
 
+  get '/teachers/new' do
+    erb :'teachers/new'
+  end
+
   get '/teachers/:teacher_name' do
     @teacher = Teacher.find_by_slug(params[:teacher_name])
     erb :'teachers/show'
-  end
-
-  get '/teachers/new' do
-    erb :'teachers/new'
   end
 
   post '/teachers' do
@@ -19,6 +19,20 @@ class TeacherController < ApplicationController
     else
       Teacher.create(params)
     end
+  end
+
+  get '/teachers/:slug/edit' do
+    @teacher = Teacher.find_by_slug(params[:slug])
+    erb :'teachers/edit'
+  end
+
+  patch '/teachers/:slug' do
+    teacher = Teacher.find_by_slug(params[:slug])
+    teacher.name = params[:name] unless params[:name].empty?
+    teacher.username = params[:username] unless params[:username].empty?
+    teacher.password = params[:password] unless params[:password].empty?
+    teacher.save
+    redirect "/teachers/#{teacher.slug}"
   end
 
 end
